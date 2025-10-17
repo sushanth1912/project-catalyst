0b9469869f7d8c16fb5cbce9e301457b83140510
Sushanth <sushanth.sc9@gmail.com>
2025-10-16 21:25:25 -0700

    Recap

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 const projectName = 'Project Catalyst';
 console.log(projectName);
+const userAge = 24;
+let isNewUser = true;
+
+const pageTitle = document.querySelector('h1');
+pageTitle.textContent = "Welcome to the Project!";
