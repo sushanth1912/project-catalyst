a96689b88e21bba6bb3e4d639321df17b8874a5f
Sushanth <sushanth.sc9@gmail.com>
2025-10-18 18:27:59 -0700

    Learning JS: Trial - Error

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ const userAge = 24;
 let isNewUser = true;
 
 const pageTitle = document.querySelector('h1');
-pageTitle.textContent = "Welcome to the Project!";
+pageTitle.textContent = 'A New Title is yet to come';
+
+const applyButton = document.querySelector('.btn');
+
+applyButton.addEventListener('click', function(){
+    console.log('The button was clicked!');
+})
+
+const profileTitle = document.querySelector('h1');
+const aboutMeParagraph = document.querySelector('p');
+
+profileTitle.addEventListener('click', function(){
+    aboutMeParagraph.textContent = 'This is my new bio';
+});
